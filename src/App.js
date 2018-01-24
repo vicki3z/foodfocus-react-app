d52fdc220653a151f5d583890dea112f18374e42
Victoria Pongsatyapipat <vpongsatyapipat@apple.com>
2018-01-24 14:51:16 +0800

    added Banners component and react-slick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header.js'
 import Main from './Main.js'
+import Banners from './Banners/Banners.js'
+import './assets/styles/libs.css';
+import './assets/styles/style.css';
 
 const App = () => (
   <div>
     <Header />
+    <Banners />
     <Main />
   </div>
 )
---
+++ src/Banners/Banners.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick'
+
+class Banners extends Component {
+	constructor() {
+		super();
+		this.state = {
+			banners: []
+		}
+	}
+	componentDidMount() {
+		fetch(`http://www.foodfocusthailand.com/wp-cms/wp-json/wp/v2/banners`)
+	    	.then(res => res.json())
+	    	.then(res => {
+	    		this.setState({
+	    			banners: res
+	    		})
+	    		console.log(this.state.banners);
+	    	})
+	}
+	renderTopBanner() {
+		return this.state.banners.map((banner,index) => (
+			<a href={banner.acf.link} title={banner.title.rendered} target="_blank" key={`banner-${index}`}>
+				<img src={banner.acf.image} alt={banner.title.rendered} />
+			</a>
+		))
+	}
+	render() {
+		var slickSettings = {
+			autoplay: true,
+	      	infinite: true,
+	      	slidesToShow: 1,
+	      	slidesToScroll: 1,
+	      	dots: true
+		}
+		return (
+			<div className="wrapper pad-top-min pad-bot-md" id="top-banner">
+				<div className="inner-container">
+	          		<div className="row">
+	            		<div className="col-sm-12">
+          					<section className="banner-wrap">
+								<Slider {...slickSettings}>
+									{this.renderTopBanner()}
+								</Slider>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default Banners;
