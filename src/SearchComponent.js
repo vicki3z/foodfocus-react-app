a3237d30725a59310934d3ab5baf79056f4117bb
victoriapong <victoria.pong@gmail.com>
2018-02-03 18:43:26 +0800

    search on Enter

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -7,21 +7,29 @@ class SearchComponent extends Component {
 			searchWord: ""
 		}
 		this.updateSearchWord = this.updateSearchWord.bind(this);
+		this._handleKeyPress = this._handleKeyPress.bind(this);
 	}
 
 	updateSearchWord(e) {
 		this.setState({searchWord: e.target.value});
 	}
+
+	_handleKeyPress(e) {
+		if (e.key === 'Enter') {
+
+	      window.location = '/search/'+this.state.searchWord;
+	    }
+	}
 	render() {
 		return (
-			<form action="#" method="get" name="search-form" id="search-form">
+			<div>
 				<a href={`/search/${this.state.searchWord}`}>
 					<img src="https://www.foodfocusthailand.com/images/icons/ic-search.svg" alt="search" />
 				</a>
-      			<div className="search-input">
-              		<input value={this.state.searchWord} type="text" name="search-input" id="search-input" onChange={this.updateSearchWord} placeholder="Search..." />
-      			</div>
-			</form>
+	  			<div className="search-input">
+	          		<input value={this.state.searchWord} type="text" onChange={this.updateSearchWord} onKeyPress={this._handleKeyPress} placeholder="Search..." />
+	  			</div>
+	  		</div>
 		)
 	}
 
