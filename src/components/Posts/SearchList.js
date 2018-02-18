ec041875780d8b94a2ed55bf0af6d0885a3764ce
victoriapong <victoria.pong@gmail.com>
2018-02-18 22:46:01 +0800

    added events to search

--- src/components/Posts/SearchList.js
+++ src/components/Posts/SearchList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HomeMainPost from '../Posts/HomeMainPost.js';
 import { Config } from "../../config.js";
+import EventItem from "../Events/EventItem.js";
 
 class SearchList extends Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      	postList: []
+	      	postList: [],
+          eventList: []
 	    }
 
   	}
@@ -49,7 +51,67 @@ class SearchList extends Component {
             this.setState({postList: res})
           }
         })
+
+      fetch(`${Config.apiUrl}/wp-json/wp/v2/events?search=${this.props.match.params.word}&order=asc&per_page=60`)
+        .then(res => res.json())
+        .then(res => {
+          this.modifyEvent(res);
+        })
   	}
+    modifyEvent(eventList){
+      var newList = []
+
+      if(eventList.length > 0){
+        var currentMonth = "";
+        var index = null;
+        eventList.map((event) => {
+          const dateStart = event.acf.date_start.split("/");
+          const dateEnd = event.acf.date_end.split("/");
+          let month = this.getMonth(dateStart[1]);
+          let endMonth = this.getMonth(dateEnd[1]);
+          event.startMonth = month;
+          event.endMonth = endMonth;
+          let year = dateStart[2];
+          if(month != currentMonth){
+            currentMonth = month;
+            newList.push({month: currentMonth, list: []});
+            index = (index != null) ? index+1 : 0;
+
+          }
+          newList[index].list.push(event)
+        })
+
+        this.setState({eventList: newList});
+      }
+    }
+    getMonth(month){
+      switch(month){
+        case "01":
+          return "January";
+        case "02":
+          return "February";
+        case "03":
+          return "March"
+        case "04":
+          return "April"
+        case "05":
+          return "May"
+        case "06":
+          return "June"
+        case "07":
+          return "July"
+        case "08":
+          return "August"
+        case "09":
+          return "September"
+        case "10":
+          return "October"
+        case "11":
+          return "November"
+        case "12":
+          return "December"
+      }
+    }
   	renderPostItem() {
       if(this.state.postList.length > 0){
         return this.state.postList.map((post,index)=>(
@@ -59,7 +121,20 @@ class SearchList extends Component {
         return null;
       }
   	}
+    renderEventItem(events) {
+      return events.map((event) => (
+        <EventItem key={`event-${event.id}`} event={event} />
+      ))
+    }
   	render() {
+      var eventList = this.state.eventList.map((eventObj,index) => (
+        <div className="col-xs-12" key={`event-obj-${index}`}>
+          <p className="text-large txt-black mar-bot">{eventObj.month.toUpperCase()}</p>
+          <ul>
+            {this.renderEventItem(eventObj.list)}
+          </ul>
+        </div>
+      ))
   		return (
         <section className="content pad-top-lg pad-bot-lg">
           <div className="inner-container">
@@ -70,6 +145,9 @@ class SearchList extends Component {
                   </div>
                   {this.renderPostItem()}
               </div>
+            </div>
+            <div className="row mar-top">
+              {eventList}
             </div>
           </div>
         </section>
