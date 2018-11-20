6f455d3dd8ce7663fd23b3d5edca8442e58d3d2d
Victoria P <victoria.pong@gmail.com>
2018-11-20 10:57:10 +0800

    Update: BottomBanner - size to 970 x 105 pixels

--- src/components/Banners/BottomBanners.js
+++ src/components/Banners/BottomBanners.js
@@ -23,36 +23,19 @@ class BottomBanners extends Component {
 		return this.state.banners.map((banner,index) => (
 			<li className="item" key={`banner-${banner.id}`}>
 				<a href={banner.acf.link} title={banner.title.rendered} target="_blank">
-					<img src={banner.acf.image} alt={banner.title.rendered} />
+					{/* <img src={banner.acf.image} alt={banner.title.rendered} /> */}
+					<span class="bottom-banner-placeholder"></span>
 				</a>
             </li>
 		))
 	}
 	render() {
 		var slickSettings = {
-			autoplay: true,
-	        mobileFirst: true,
-	        variableWidth: true,
-	        slidesToShow: 4,
-	        slidesToScroll: 2,
-	        responsive: [{
-	            breakpoint: 768,
-	            settings: {
-	            	infinite: true,
-	                slidesToShow: 4,
-	                slidesToScroll: 2,
-	                dots: false
-	            }
-	        },
-	        {
-	            breakpoint: 480,
-	            settings: {
-	              	infinite: true,
-	              	slidesToShow: 2,
-	              	slidesToScroll: 1,
-	              	dots: false
-	            }
-	        }]
+			dots: false,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true
 		}
 		return (
 			<div className="col-sm-12">
